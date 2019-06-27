03b3fa03b28d6490eadbfd634becbf971c9a6cf3
Paulo <paulokucher@brq.com>
2019-06-27 11:34:15 -0300

    Initial Commit

---
+++ CardTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardTraining
+//
+//  Created by brq on 27/06/2019.
+//  Copyright © 2019 brq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
